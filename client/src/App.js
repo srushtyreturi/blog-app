1e60b82b528f04f06a9a349ea79b38b4592bfcb4
SrushtyR <srushtyr@Srushtys-MacBook-Air.local>
2024-01-21 19:37:00 -0500

    Created basic blog app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/01/GettyImages-1256204431.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>The two faces of AI</h2>
+          <p className="info">
+            <span className="author">Srushty R</span>
+            <time>2024-01-21 16:47</time>
+          </p>
+          <p className="summary">
+            We all make mistakes. But sometimes we forget that technology does,
+            too — especially when it comes to AI, which is still in its early
+            days in many respects. — Anna
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/01/GettyImages-1256204431.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>The two faces of AI</h2>
+          <p className="info">
+            <span className="author">Srushty R</span>
+            <time>2024-01-21 16:47</time>
+          </p>
+          <p className="summary">
+            We all make mistakes. But sometimes we forget that technology does,
+            too — especially when it comes to AI, which is still in its early
+            days in many respects. — Anna
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/01/GettyImages-1256204431.jpg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>The two faces of AI</h2>
+          <p className="info">
+            <span className="author">Srushty R</span>
+            <time>2024-01-21 16:47</time>
+          </p>
+          <p className="summary">
+            We all make mistakes. But sometimes we forget that technology does,
+            too — especially when it comes to AI, which is still in its early
+            days in many respects. — Anna
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
